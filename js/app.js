4649cef6ec4fd0cb4b17ed2655919349642eca01
Cristixndr3s <cristiandres17@gmail.com>
2020-06-23 23:07:57 -0500

    fin proyecto

--- js/app.js
+++ js/app.js
@@ -29,15 +29,8 @@ function init() {
 
 function createCamera() {
 
-  camera = new THREE.PerspectiveCamera(
-    35, // FOV
-    container.clientWidth / container.clientHeight, // aspect
-
-    0.1, // near clipping plane
-    100, // far clipping plane
-  );
-
-  camera.position.set( -4, 4, 10 );
+  camera = new THREE.PerspectiveCamera( 35, container.clientWidth / container.clientHeight, 0.1, 100 );
+  camera.position.set( -5, 5, 7 );
 
 }
 
@@ -62,24 +55,98 @@ function createLights() {
 
 }
 
+function createMaterials() {
+
+  // we'll create a red materials for the body
+  // and a dark grey material for the details here
+  const body = new THREE.MeshStandardMaterial( {
+  color: 0xff3333, // red
+  flatShading: true,
+} );
+
+// just as with textures, we need to put colors into linear color space
+body.color.convertSRGBToLinear();
+
+const detail = new THREE.MeshStandardMaterial( {
+  color: 0x333333, // darkgrey
+  flatShading: true,
+} );
+
+detail.color.convertSRGBToLinear();
+
+return {
+
+  body,
+  detail,
+
+};
+
+
+}
+
+function createGeometries() {
+
+  // we'll create geometries for the nose, cabin, chimney, and wheels here
+   const nose = new THREE.CylinderBufferGeometry( 0.75, 0.75, 3, 12 );
+   const cabin = new THREE.BoxBufferGeometry( 2, 2.25, 1.5 );
+   const chimney = new THREE.CylinderBufferGeometry( 0.3, 0.1, 0.5 );
+   const wheel = new THREE.CylinderBufferGeometry( 0.4, 0.4, 1.75, 16 );
+   wheel.rotateX( Math.PI / 2 );
+
+
+   return{
+    nose,
+    cabin,
+    chimney,
+    wheel,
+   };
+
+}
+
 function createMeshes() {
 
-  const geometry = new THREE.BoxBufferGeometry( 2, 2, 2 );
+  const train = new THREE.Group();
+  scene.add( train );
+  const materials = createMaterials();
+  const geometries = createGeometries();
 
-  const textureLoader = new THREE.TextureLoader();
+  const nose = new THREE.Mesh( geometries.nose, materials.body );
+  nose.rotation.z = Math.PI / 2;
+  nose.position.x = -1;
 
-  const texture = textureLoader.load( 'textures/uv_test_bw.png' );
+  const cabin = new THREE.Mesh( geometries.cabin, materials.body );
+  cabin.position.set( 1.5, 0.4, 0 );
 
-  texture.encoding = THREE.sRGBEncoding;
-  texture.anisotropy = 16;
+  const chimney = new THREE.Mesh( geometries.chimney, materials.detail );
+  chimney.position.set( -2, 0.9, 0 );
 
-  const material = new THREE.MeshStandardMaterial( {
-    map: texture,
-  } );
+  const smallWheelRear = new THREE.Mesh( geometries.wheel, materials.detail );
+  smallWheelRear.position.set( 0, -0.5, 0 );
 
-  mesh = new THREE.Mesh( geometry, material );
+  const smallWheelCenter = smallWheelRear.clone();
+  smallWheelCenter.position.x = -1;
 
-  scene.add( mesh );
+  const smallWheelFront = smallWheelRear.clone();
+  smallWheelFront.position.x = -2;
+
+  const bigWheel = smallWheelRear.clone();
+  bigWheel.scale.set( 2, 2, 1.25 );
+  bigWheel.position.set( 1.5, -0.1, 0 );
+
+  train.add(
+
+  nose,
+  cabin,
+  chimney,
+
+  smallWheelRear,
+  smallWheelCenter,
+  smallWheelFront,
+  bigWheel,
+
+);
+
+
 
 }
 
